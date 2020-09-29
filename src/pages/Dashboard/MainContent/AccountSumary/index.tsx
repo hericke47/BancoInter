c356056ad470d06cc06e114eaa3491977f816712
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-28 22:52:44 -0300

    :sparkles: feat: add line chart to account sumary

--- src/pages/Dashboard/MainContent/AccountSumary/index.tsx
+++ src/pages/Dashboard/MainContent/AccountSumary/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
 import { FiCreditCard, FiFileText, FiEye, FiEyeOff } from 'react-icons/fi';
+import { ResponsiveLine } from '@nivo/line';
+import { useTheme } from 'styled-components';
 
+import { lineChartData } from '../../../../resources';
 import {
   Container,
   Card,
@@ -10,14 +13,26 @@ import {
   LeftData,
   RightData,
   DataValue,
+  CustomTooltip,
 } from './styles';
 import { PlataformaPaiIcon } from '../../../../assets/images/icons';
 import CreditCardIllustration from '../../../../assets/images/illustrations/card-illustration.png';
 import Button from '../../../../components/Button';
 
-const AccountSumary: React.FC = () => {
+type ChartValue = number | React.ReactText | undefined;
+
+const formatChartValue = (value: ChartValue): string => `${value || 0}%`;
+
+const AccountSummary: React.FC = () => {
   const [displayStatement, setDisplayStatement] = useState(true);
-  const [displayInvestiments, setDisplayInvestiments] = useState(true);
+  const [displayInvestments, setDisplayInvestments] = useState(true);
+  const [investmentGrowth, setInvestmentGrowth] = useState(() => {
+    const [investments] = lineChartData;
+    const { y } = investments.data[investments.data.length - 1];
+    return formatChartValue(y);
+  });
+
+  const { colors } = useTheme();
 
   return (
     <Container>
@@ -26,64 +41,106 @@ const AccountSumary: React.FC = () => {
           <FiFileText />
           <h3>Extrato</h3>
           <Button
-            onClick={() => setDisplayStatement((prevstate) => !prevstate)}
+            onClick={() => setDisplayStatement((prevState) => !prevState)}
             variant="transparent"
           >
             {displayStatement ? <FiEyeOff /> : <FiEye />}
           </Button>
         </Header>
+
         <DataWrapper>
-          <LeftData>Grafico</LeftData>
+          <LeftData>Gráfico</LeftData>
           <RightData>
             <span>Receita</span>
             <DataValue income>
               {displayStatement ? 'R$ 8.552,22' : '---'}
             </DataValue>
-            <span>Dispesas</span>
+            <span>Despesas</span>
             <DataValue outcome>
               {displayStatement ? 'R$ 7.948,55' : '---'}
             </DataValue>
           </RightData>
         </DataWrapper>
       </Card>
-
       <Card>
         <Header iconStroke>
           <FiCreditCard />
-          <h3>Mastercard 8430</h3>
+          <h3>MasterCard 8430</h3>
         </Header>
+
         <DataWrapper>
           <LeftData>
             <img
               src={CreditCardIllustration}
-              alt="Cartão de credito sem anuidade"
+              alt="Cartão de Crédito Sem Anuidade"
             />
           </LeftData>
           <RightData>
-            <span>Seu cartão é Mastercard e sem anuidade!</span>
+            <span>Seu cartão é MasterCard e sem anuidade!</span>
           </RightData>
         </DataWrapper>
       </Card>
-
       <Card>
         <Header iconStroke={false}>
           <PlataformaPaiIcon />
           <h3>Plataforma Aberta Inter</h3>
           <Button
-            onClick={() => setDisplayInvestiments((prevstate) => !prevstate)}
+            onClick={() => setDisplayInvestments((prevState) => !prevState)}
             variant="transparent"
           >
-            {displayInvestiments ? <FiEyeOff /> : <FiEye />}
+            {displayInvestments ? <FiEyeOff /> : <FiEye />}
           </Button>
         </Header>
 
         <DataWrapper>
-          <LeftData>Grafico</LeftData>
+          <LeftData>
+            <ResponsiveLine
+              data={lineChartData}
+              useMesh
+              enableArea
+              enableCrosshair={false}
+              curve="cardinal"
+              margin={{ top: 8, right: 8, bottom: 20, left: 8 }}
+              xScale={{ type: 'point' }}
+              yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false,
+              }}
+              tooltip={({ point }) => {
+                return (
+                  <CustomTooltip>
+                    {formatChartValue(point.data.yFormatted)}
+                  </CustomTooltip>
+                );
+              }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                orient: 'bottom',
+                tickSize: 0,
+                tickPadding: 8,
+                tickRotation: 0,
+              }}
+              axisLeft={null}
+              colors={colors.success}
+              lineWidth={1.5}
+              pointSize={8}
+              pointColor={colors.success}
+              pointLabel="y"
+              pointLabelYOffset={-12}
+              enableGridY={false}
+            />
+          </LeftData>
           <RightData>
-            <span>Tota investido</span>
-            <DataValue>{displayInvestiments ? 'R$ 5.750,00' : '---'}</DataValue>
-            <span>Evolução no Mês</span>
-            <DataValue>{displayInvestiments ? '20%' : '---'}</DataValue>
+            <span>Total investido</span>
+            <DataValue>{displayInvestments ? 'R$ 5.750,00' : '---'}</DataValue>
+            <span>Evolução no mês</span>
+            <DataValue>
+              {displayInvestments ? investmentGrowth : '---'}
+            </DataValue>
           </RightData>
         </DataWrapper>
       </Card>
@@ -91,4 +148,4 @@ const AccountSumary: React.FC = () => {
   );
 };
 
-export default AccountSumary;
+export default AccountSummary;
--- src/pages/Dashboard/MainContent/AccountSumary/styles.ts
+++ src/pages/Dashboard/MainContent/AccountSumary/styles.ts
@@ -8,8 +8,8 @@ export const Container = styled.section`
     grid-template-columns: repeat(3, minmax(0, 1fr));
     column-gap: 2.4rem;
     margin: 2.4rem 0;
-    height: 100%;
     max-height: 17.6rem;
+    height: 100%;
   `}
 `;
 
@@ -17,8 +17,8 @@ export const Card = styled.article`
   ${({ theme }) => css`
     background: ${theme.colors.background};
     max-width: 26.4rem;
-    height: 100%;
     width: 100%;
+    height: 100%;
     border-radius: ${theme.radius.default};
     padding: 1.6rem 2.4rem;
     box-shadow: ${theme.shadows.default};
@@ -32,27 +32,23 @@ export const Header = styled.div<{ iconStroke?: boolean }>`
     display: flex;
     justify-content: flex-start;
     margin-bottom: 0.8rem;
-
-    svg {
+    > svg {
       margin-right: 0.8rem;
       stroke: ${iconStroke && theme.colors.primary};
       width: auto;
       height: 1.6rem;
     }
-
     > h3 {
       font-weight: 500;
-      stroke: ${theme.colors.darkGrey};
+      color: ${theme.colors.darkGrey};
       font-size: ${theme.fontSizes.small};
     }
-
     > button {
       margin: -0.8rem -1.6rem 0 auto;
       padding: 0.8rem 1.6rem;
-
       svg {
         stroke: ${theme.colors.grey};
-        stroke-width: 2;
+        stroke-width: 1.5;
       }
     }
   `}
@@ -67,17 +63,23 @@ export const DataWrapper = styled.div`
 `;
 
 export const LeftData = styled.div`
-  ${() => css`
+  ${({ theme }) => css`
     margin-right: 0.8rem;
     max-width: 55%;
+    max-height: 9.6rem;
     width: 100%;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-
     > img {
       height: 8.4rem;
       width: auto;
+    }
+    svg text {
+      font-family: inherit !important;
+      font-size: ${theme.fontSizes.tiny} !important;
+      fill: ${theme.colors.grey} !important;
     }
   `}
 `;
@@ -85,7 +87,6 @@ export const LeftData = styled.div`
 export const RightData = styled.div`
   ${({ theme }) => css`
     font-size: ${theme.fontSizes.small};
-
     > span:not(:only-child):last-of-type {
       margin-top: 0.8rem;
       display: block;
@@ -113,6 +114,34 @@ const getDataColor = (
 
 export const DataValue = styled.div<DataValueType>`
   ${({ theme, income, outcome }) => css`
-    color: ${getDataColor(theme.colors, { income, outcome })}};
+    color: ${getDataColor(theme.colors, { income, outcome })};
+  `}
+`;
+
+export const CustomTooltip = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.lightGrey};
+    padding: 0.4rem 0.8rem;
+    border-radius: ${theme.radius.small};
+    font-size: ${theme.fontSizes.tiny};
+    text-align: center;
+    box-shadow: 0 -0.1rem 0.2rem rgba(0, 0, 0, 0.28);
+    :before {
+      content: '';
+      display: block;
+      height: 0;
+      left: 50%;
+      bottom: -50%;
+      position: absolute;
+      transform: translate3d(-50%, 25%, 0);
+      border-color: ${theme.colors.lightGrey} transparent transparent
+        transparent;
+      border-style: solid;
+      border-width: 0.8rem;
+    }
+    :after {
+      content: '';
+      display: block;
+    }
   `}
 `;
