2a324a854f6e30e1cf624b996dac66b70f3c93a2
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-09 03:39:14 -0300

    :fire: feat: add helpDesk component

--- src/pages/Dashboard/SideBar/index.tsx
+++ src/pages/Dashboard/SideBar/index.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 
 import { FiBarChart2 } from 'react-icons/fi';
 
-import Dropdown from './Dropdown';
+import { Wrapper, Container, AmountInvested } from './styles';
 import { sideBarNavigation } from '../../../resources';
-import { Container, AmountInvested } from './styles';
+import Dropdown from './Dropdown';
 import AccountBalance from './AccountBalance';
+import HelpDesk from './HelpDesk';
 
 const SideBar: React.FC = () => {
   return (
-    <Container>
-      <AccountBalance />
-      <AmountInvested>
-        Ver total investido <FiBarChart2 size="1.6rem" />
-      </AmountInvested>
-      {/* o entries ele pega as 'enradas' do objeto e transforma em um array */}
-      {Object.entries(sideBarNavigation).map(([key, value]) => (
-        <Dropdown key={key} icon={value.icon} sectionName={key} />
-      ))}
-    </Container>
+    <Wrapper>
+      <Container>
+        <AccountBalance />
+        <AmountInvested>
+          Ver Total Investido <FiBarChart2 size="1.6rem" />
+        </AmountInvested>
+        {Object.entries(sideBarNavigation).map(([key, value]) => (
+          <Dropdown key={key} icon={value.icon} sectionName={key} />
+        ))}
+      </Container>
+      <HelpDesk />
+    </Wrapper>
   );
 };
 
